24da151776b1d6414450a8c23fe0e4c03ece859d
张洪财 <371494097@qq.com>
2018-03-30 11:31:30 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            ping02("heart.bchunk.com");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        System.out.println("Hello Wossrld!" + StringUtils.trim(" aaaa "));
+    }
+    public static void ping02(String ipAddress) throws Exception {
+        String line = null;
+        try {
+            Process pro = Runtime.getRuntime().exec("ping " + ipAddress);
+            BufferedReader buf = new BufferedReader(new InputStreamReader(
+                    pro.getInputStream()));
+            while ((line = buf.readLine()) != null)
+                System.out.println(line);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
